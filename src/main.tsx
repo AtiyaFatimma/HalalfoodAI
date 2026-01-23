eba42634b5fb88cf04730c081697002119f98277
AtiyaFatimma <149862137+AtiyaFatimma@users.noreply.github.com>
2026-01-23 13:19:29 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css' 
+import './index.css'
 
-// This connects the React code to the <div id="root"></div> in your index.html
-const rootElement = document.getElementById('root');
-
-if (!rootElement) {
-  throw new Error("Failed to find the root element. Make sure index.html has <div id='root'></div>");
-}
+// Connect React to the <div id="root"></div> in index.html
+const rootElement = document.getElementById('root') as HTMLElement
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
