097c5abf0ce2d69a0504bdcc626910aa27f5f50d
AtiyaFatimma <149862137+AtiyaFatimma@users.noreply.github.com>
2026-01-23 12:44:40 +0100

    Add error handling for root element in entry.tsx

--- src/entry.tsx
+++ src/entry.tsx
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css' 
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// This connects the React code to the <div id="root"></div> in your index.html
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error("Failed to find the root element. Make sure index.html has <div id='root'></div>");
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
